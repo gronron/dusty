185ead6f96d452135e1e17dd82f2541b92c69485
geoffrey.touron <geoffrey.touron@gmail.com>
2015-07-06 05:33:30 +0200

    add body.

---
+++ physic/body.hpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+Copyright (c) 2015, Geoffrey TOURON
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of dusty nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#ifndef BODY_H_
+#define BODY_H_
+
+#include "math/vec.hpp"
+#include "endian/packet.hpp"
+
+struct	Body
+{
+	enum Type { NONE = 0, ALLOCATED = 1, COLLISIONABLE = 2 };
+	
+	Type	type;
+
+	union
+	{
+		Actor		*actor;
+		int			next;
+	};
+
+	int				index;
+
+	float			mass;
+	vec<float, 4>	position;
+	vec<float, 4>	velocity;
+	vec<float, 4>	acceleration;
+
+	vec<float, 4>	size;
+	vec<float, 4>	nextposition;
+	vec<float, 4>	nextvelocity;
+
+
+	void	get_replication(Packet &) const;
+	void	replicate(Packet &, float);
+};
+
+#endif
